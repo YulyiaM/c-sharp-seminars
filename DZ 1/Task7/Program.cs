529b8836d506bc2bef9446ffe3d5ab2b021a0527
Xaliffa <xaliffa@gmail.com>
2022-11-27 20:14:51 +0300

    task 7

---
+++ DZ 1/Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the integer: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = 0;
+
+if (number / 100 == 0)
+{
+    Console.WriteLine("There is no third digit from left");
+}
+
+else
+{
+    while (number >= 1000)
+    {
+        number /= 10;
+    }
+    thirdDigit = number % 10;
+    Console.WriteLine("The third digit from left is: " + thirdDigit);
+}
