c650cae8108176eb5f658b8ae5f99a39fc6692a8
Xaliffa <xaliffa@gmail.com>
2022-11-27 19:09:45 +0300

    task 4 and task 6

---
+++ DZ 1/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter the first number: ");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the third number: ");
+double thirdNumber = Convert.ToDouble(Console.ReadLine());
+
+double max = firstNumber;
+
+if ((secondNumber > max) & (secondNumber > thirdNumber))
+{
+    max = secondNumber;
+}
+else if ((thirdNumber > max) & (thirdNumber > secondNumber))
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine($"Max value is {max}");
---
+++ DZ 1/Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the integer: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} is even!");
+}
+else 
+{
+    Console.WriteLine($"{number} is odd!");
+}
