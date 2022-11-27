11f58b70dea9793472cbdfa86dba6f019fdd14dc
Xaliffa <xaliffa@gmail.com>
2022-11-27 18:10:51 +0300

    tasks about -N:N and 3-digital number

---
+++ Sem1/2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter the integer N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = -N;
+while (count <= N)
+{
+    Console.Write($"{count} ");
+    count++;
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Enter the three-digit number:");
+int threeDigitN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(threeDigitN % 10);
