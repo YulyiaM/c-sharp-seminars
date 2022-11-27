94849b888fd3cc3ab28b5bd4efa23e805f1a3c08
Xaliffa <xaliffa@gmail.com>
2022-11-27 18:33:26 +0300

    task 2

---
+++ DZ 1/Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Enter the first integer a = ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a second integer different from the first b = ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"max = {firstNumber}");
+}
+else
+{
+    Console.WriteLine($"max = {secondNumber}");
+}
