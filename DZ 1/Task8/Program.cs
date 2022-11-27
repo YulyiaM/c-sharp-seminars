6a0f2e030dae7c9af722eb6ca3adf70c41fc55ec
Xaliffa <xaliffa@gmail.com>
2022-11-27 19:28:01 +0300

    task 8

---
+++ DZ 1/Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the integer > 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+while (count <= N)
+{
+        Console.Write(count + " ");
+        count += 2;
+}
