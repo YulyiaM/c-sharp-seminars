6e8e89fbf7185a18bd6484705433600513f8ea10
Xaliffa <xaliffa@gmail.com>
2023-01-08 06:56:53 +0300

    Multiply or remainder

---
+++ Sem2/Task12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int remainNumber = firstNumber % secondNumber;
+
+if (remainNumber == 0) Console.WriteLine("The first number is a multiple of the second");
+else Console.WriteLine($"The remainder of the division is {remainNumber}");
