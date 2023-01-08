6cb96ff7f695f91e3f199685bcbb6e99387d8b11
Xaliffa <xaliffa@gmail.com>
2023-01-08 20:31:07 +0300

    Is Day Off

---
+++ DZ2/Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Enter the number from 1 to 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 8) Console.WriteLine(IfDayOff(number) ? "Yes, the day is off" : "No, it is working day");
+else Console.WriteLine("Incorrect number!");
+
+Console.WriteLine();
+
+bool IfDayOff(int num)
+{
+    return num == 6 || num == 7;
+}
