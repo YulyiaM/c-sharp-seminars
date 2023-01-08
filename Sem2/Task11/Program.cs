90979df1eda42d9beb432f8aaef4f369baa367d5
Xaliffa <xaliffa@gmail.com>
2023-01-08 06:41:21 +0300

    Without second number

---
+++ Sem2/Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Number = {number}");
+
+int firstDigit = number / 100;
+// Console.WriteLine(firstDigit);
+int lastDigit = number % 10;
+// Console.WriteLine(thirdDigit);
+
+int newNumber = firstDigit * 10 + lastDigit;
+Console.WriteLine($"New number = {newNumber}");
