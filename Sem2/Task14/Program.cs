a3ffab6929c5a98fe6ab0feb193e378efb305865
Xaliffa <xaliffa@gmail.com>
2023-01-08 07:41:04 +0300

    Multiply 7 and 23

---
+++ Sem2/Task14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Multiply(number));
+
+bool Multiply(int num)
+{
+    if (num%7==0 | num%23==0) return true;
+    else return false;
+}
