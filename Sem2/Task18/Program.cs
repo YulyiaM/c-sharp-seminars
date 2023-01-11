23c3da545633508f612aa6ed29d99728131927b3
Xaliffa <xaliffa@gmail.com>
2023-01-11 20:49:18 +0300

    Range to quarter

---
+++ Sem2/Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string range = QuarterCoord(num);
+Console.WriteLine(range);
+
+string QuarterCoord(int number)
+{
+    if (number == 1) return "Диапазон координат: x > 0, y > 0";
+    if (number == 2) return "Диапазон координат: x < 0, y > 0";
+    if (number == 3) return "Диапазон координат: x < 0, y < 0";
+    if (number == 4) return "Диапазон координат: x > 0, y < 0";
+    return "Введены некорректные данные";
+} 
