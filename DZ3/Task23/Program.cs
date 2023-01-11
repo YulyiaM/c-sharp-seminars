9a15b288887fde76c5d6d20d98d310912388def3
Xaliffa <xaliffa@gmail.com>
2023-01-12 00:28:12 +0300

    Cubes

---
+++ DZ3/Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿void Cubes(int num)
+{
+    int counter = 1;
+    while (counter <= num)
+    {
+        Console.WriteLine($"{counter,7} {counter * counter * counter,7}");
+        counter++;
+    }
+}
+
+Console.WriteLine("Enter the natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 1)
+{
+    Console.WriteLine("Enter the NATURAL number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+Cubes(number);
