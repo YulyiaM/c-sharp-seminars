0d23e6dec42b4ca4068326e79f9d07a81a12892b
Xaliffa <xaliffa@gmail.com>
2023-01-18 23:09:58 +0300

    Random 0 1

---
+++ Sem4/Task30/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 2);
+//     Console.Write(array[i] + " ");
+// }
+
+void FillArray(int[] collection)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = rnd.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
