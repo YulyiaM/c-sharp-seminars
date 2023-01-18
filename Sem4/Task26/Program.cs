d11712cb13bc595600f85ada1d7a8b46bcd4d88b
Xaliffa <xaliffa@gmail.com>
2023-01-18 16:58:07 +0300

    Quantity of digits

---
+++ Sem4/Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Quantity of digits in {number} is {QuantityOfDigits(number)}");
+
+int QuantityOfDigits(int num)
+{
+    num = Math.Abs(num);
+    int quantityOfDigits = 1;
+    while (num / 10 > 0)
+    {
+        num = num / 10;
+        quantityOfDigits++;
+    }
+    return quantityOfDigits;
+}
