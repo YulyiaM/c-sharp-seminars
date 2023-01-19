0fe608d66631b7e95553828178dd32f90a882e1a
Xaliffa <xaliffa@gmail.com>
2023-01-20 00:30:31 +0300

    Invertioned array

---
+++ Sem5/Task32/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int[] ArrayInvertion(int[] array)
+{
+    int[] invertionedArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        invertionedArray[i] = array[i]*(-1);
+    }
+    return invertionedArray;
+}
+
+int[] arr = CreateArrayRndInt(4, -9, 9);
+PrintArray(arr);
+Console.Write(" -> ");
+int[] invertionedArr = ArrayInvertion(arr);
+PrintArray(invertionedArr);
+
