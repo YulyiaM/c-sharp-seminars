9f2ef0ba0c469f4d264aeaa1fa7fdac1d883b942
Xaliffa <xaliffa@gmail.com>
2023-01-20 01:03:09 +0300

    IsNumberInArray

---
+++ Sem5/Task33/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+bool IsNumberInArray(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArrayRndInt(10, 0, 10);
+bool yesNo = IsNumberInArray(arr, number);
+PrintArray(arr);
+Console.Write(" -> ");
+Console.WriteLine(yesNo ? "Yes" : "No");
+
+
