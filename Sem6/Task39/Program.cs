302254f40ecce345ff60225d8542ca4f94fad549
Xaliffa <xaliffa@gmail.com>
2023-01-21 20:31:07 +0300

    Reverse array

---
+++ Sem6/Task39/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateArray(5, 1, 10);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
+Array.Reverse(array);
+PrintArray(array);
