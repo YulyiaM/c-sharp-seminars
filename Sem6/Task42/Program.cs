47989fb4d663eb0dfc2b46aa264f607f6fb2a2c4
Xaliffa <xaliffa@gmail.com>
2023-01-21 22:22:13 +0300

    Decimal to binary

---
+++ Sem6/Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿string DecimalToBinary(int decNum)
+{
+    string result = String.Empty;
+
+    while (decNum > 0)
+    {
+        result = decNum % 2 + result;
+        decNum = decNum / 2;
+    }
+    
+    return result;
+    // string reverseResult = String.Empty;
+    // for (int i = 0; i < result.Length; i++)
+    // {
+    //     reverseResult += result[result.Length - i - 1];
+    // }
+    //  return Convert.ToInt32(reverseResult);
+}
+
+Console.WriteLine("Enter the decimal number: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{decimalNumber} is {DecimalToBinary(decimalNumber)} in binary system");
+
