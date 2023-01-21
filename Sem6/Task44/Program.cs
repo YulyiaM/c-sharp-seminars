ec0fa182bc5ef11e8458bf7ec831ce3b94ccad78
Xaliffa <xaliffa@gmail.com>
2023-01-21 23:27:07 +0300

    Fibonacci numbers

---
+++ Sem6/Task44/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter the number N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] fiboNums = FibonacciNumbers(number);
+PrintArray(fiboNums);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] FibonacciNumbers(int num)
+{
+    int[] fibonacciNumbers = new int[num];
+    //fibonacciNumbers[0] = 0; по умолчанию так и есть
+    fibonacciNumbers[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+    }
+    return fibonacciNumbers;
+}
+
