88f0f7971230f270cd0901a29ec211cdd3a5a3db
Xaliffa <xaliffa@gmail.com>
2023-01-21 23:26:09 +0300

    Array copy

---
+++ Sem6/Task45/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+}
+
+int[] ArrayCopy(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+    return arrayCopy;
+}
+
+int[] arr = CreateArray(8, 0, 100);
+PrintArray(arr);
+int[] arrCopy = ArrayCopy(arr);
+PrintArray(arrCopy);
