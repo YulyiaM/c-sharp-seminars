7f01e71f54c1f96c6c631757e96ee55a2f2f4d99
Xaliffa <xaliffa@gmail.com>
2023-02-05 17:02:43 +0300

    Random Matrix

---
+++ Sem7/Task46/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] CreateRandomArray(int min, int max, int rows, int columns)
+{
+    Random rnd = new Random();
+    int[,] randomArray = new int[rows, columns];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            randomArray[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return randomArray;
+}
+
+void PrintArray(int[,] arr)
+{    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] resultArray = CreateRandomArray(-100, 100, 3, 4);
+PrintArray(resultArray);
