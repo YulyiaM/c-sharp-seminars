0b050647fa501e9e2051ea8fcc0dfc8c6597aeb4
Xaliffa <xaliffa@gmail.com>
2023-02-05 22:51:03 +0300

    53,55,55-2

---
+++ Sem8/Task53/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] CreateRandomMatrix(int rows, int columns, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] randomMatrix = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            randomMatrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return randomMatrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void ChangeMatrixRows(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+
+int[,] newMatrix = CreateRandomMatrix(4, 7, 0, 10);
+PrintMatrix(newMatrix);
+Console.WriteLine("The new matrix: ");
+ChangeMatrixRows(newMatrix);
+PrintMatrix(newMatrix);
+
+
+
