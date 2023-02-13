b334829cfb4e5ea59f37228008a9fbb700aa5a8d
Xaliffa <xaliffa@gmail.com>
2023-02-13 03:47:13 +0300

    Recursion: numbers from M to N

---
+++ Sem9/Task65/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter the natural numbers");
+Console.Write("N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write("M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(numberN, numberM);
+
+void NaturalNumbers(int numN, int numM)
+{
+    if (numN == numM)
+    {
+        Console.Write($"{numN} ");
+        return;
+    }
+
+    if (numN > numM)
+        NaturalNumbers(numN - 1, numM);
+    else
+        NaturalNumbers(numN + 1, numM);
+    Console.Write($"{numN} ");
+}
