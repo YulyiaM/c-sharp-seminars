49a4707e61fb688962a9ea1311d21d5b933f36af
Xaliffa <xaliffa@gmail.com>
2023-02-13 09:44:18 +0300

    Natural numbers summ(M-N)

---
+++ DZ9/Task66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int EnteringIntegers(string message)
+{
+    int result;
+    bool parse;
+    Console.Write(message);
+    parse = Int32.TryParse(Console.ReadLine(), out result);
+    if (!parse) result = EnteringIntegers(message);
+    return result;
+}
+
+int NaturalNumbersSumm(int numM, int numN)
+{
+    if (numM == numN) return numM; //не придумала, как корректно вывести сумму при равенстве M и N
+
+    if (numM < numN) return numN + NaturalNumbersSumm(numM, numN - 1);
+
+    else return numM + NaturalNumbersSumm(numM - 1, numN);
+}
+
+int numberM = EnteringIntegers("Enter the natural number M: ");
+int numberN = EnteringIntegers("Enter the natural number N: ");
+
+Console.Write($"Summ = {NaturalNumbersSumm(numberM, numberN)}");
